d8486145aa74938bc784373cc8b114750b6d51b4
Nazib910 <sayednajib192@gmail.com>
2024-02-14 13:43:18 +0600

    new up

---
+++ Milestone 4/Module 16/for_loop.js
@@ -0,0 +1,10 @@
+//  for(let num = 0; num < 5; num++){
+//      console.log("i am going to work had for the next three months")
+//  }
+
+for(let i = 0; i < 100; i++) {
+    if(i%2 !== 0) {
+        console.log(i)
+    }
+    
+} 
---
+++ Milestone 4/Module 19/string.js
@@ -0,0 +1,7 @@
+// String is immutable unlike numbers   
+// Array is mutable
+
+
+const school = 'Rajuk uttara model school';
+
+// console.log(school);
